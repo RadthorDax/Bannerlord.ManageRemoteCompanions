2a9a9ea88a7dc774d6f2ce657d1d87eaa604737a
RadthorDax <RadthorDax@users.noreply.github.com>
2020-04-18 23:19:54 +0100

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Windows.Forms;
+using TaleWorlds.MountAndBlade;
+
+namespace UpgradeRemoteCompanions
+{
+    internal class Main : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            try
+            {
+                new Harmony("com.radthordax.bannerlord.UpgradeRemoteCompanions").PatchAll();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(string.Format("Exception applying UpgradeRemoteCompanions Harmony patch:\n{0}", e.ToString()));
+            }
+        }
+    }
+}
---
+++ PatchSkillVM.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.CampaignSystem.ViewModelCollection.CharacterDeveloper;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+
+namespace UpgradeRemoteCompanions
+{
+    [HarmonyPatch(typeof(SkillVM), "IsPerkAvailable")]
+    internal class PatchIsPerkAvailable
+    {
+        public static void Postfix(SkillVM __instance, PerkObject perk, ref bool __result)
+        {
+            __result = perk.RequiredSkillValue <= __instance.Level;
+        }
+    }
+
+    [HarmonyPatch(typeof(CampaignUIHelper), "GetAddFocusHintString")]
+    internal class PatchRefreshCanAddFocus
+    {
+        public static void Prefix(ref bool isInSamePartyAsPlayer)
+        {
+            isInSamePartyAsPlayer = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterVM), "CanAddFocusToSkillWithFocusAmount")]
+    internal class PatchSkillFocus
+    {
+        public static void Postfix(CharacterVM __instance, int currentFocusAmount, ref bool __result)
+        {
+            __result = currentFocusAmount < 5 && __instance.UnspentCharacterPoints > 0;
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterAttributeItemVM), "RefreshWithCurrentValues")]
+    internal class PatchAttributePoints
+    {
+        public static void Postfix(CharacterAttributeItemVM __instance)
+        {
+            __instance.CanAddPoint = __instance.AttributeValue < 10 && __instance.UnspentAttributePoints > 0;
+        }
+    }
+}
