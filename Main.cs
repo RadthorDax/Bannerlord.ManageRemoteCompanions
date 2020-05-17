47278d3553c752c8464d7e5764b4572096b34211
RadthorDax <RadthorDax@users.noreply.github.com>
2020-05-17 22:39:46 +0100

    wip

--- Main.cs
+++ Main.cs
@@ -1,7 +1,18 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
+using SandBox.GauntletUI;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.Engine.Screens;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
+using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement.Categories;
+using System.Reflection;
+using TaleWorlds.Library;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 
 namespace ManageRemoteCompanions
 {
@@ -19,6 +30,39 @@ namespace ManageRemoteCompanions
             {
                 MessageBox.Show(string.Format("Exception applying ManageRemoteCompanions Harmony patch:\n{0}", e.ToString()));
             }
+
+            //ScreenManager.OnPushScreen += new ScreenManager.OnPushScreenEvent(OnPushScreen);
         }
+
+        //protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        //{
+        //    if (!(game.GameType is Campaign))
+        //        return;
+
+        //    if (!(gameStarterObject.Models is IList<GameModel> models))
+        //        return;
+
+        //    for (int i = 0; i < models.Count; ++i)
+        //        if (models[i] is ClanPartiesVM)
+        //            models[i] = Activator.CreateInstance<ClanPartiesManageTroopsVM>();
+
+        //}
+
+        //private void OnPushScreen(ScreenBase screen)
+        //{
+        //    if (!(screen is GauntletClanScreen))
+        //        return;
+
+        //    ClanVM clanVM = (ClanVM)typeof(GauntletClanScreen).GetField("_dataSource", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(screen);
+        //    ClanPartiesVM clanPartiesVM = (ClanPartiesVM)typeof(ClanVM).GetField("_clanParties", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(clanVM);
+        //    ManageTroopsVM manageTroopsVM = new ManageTroopsVM(clanPartiesVM);
+
+        //    GauntletLayer layer = new GauntletLayer(288);
+        //    layer.Name = "ManageTroops";
+        //    layer.LoadMovie("ManageTroops", manageTroopsVM);
+        //    layer.InputRestrictions.SetInputRestrictions(false, InputUsageMask.MouseButtons);
+
+        //    screen.AddLayer(layer);
+        //}
     }
 }
--- PatchClanPartiesVM.cs
+++ PatchClanPartiesVM.cs
@@ -1,9 +1,17 @@
 ﻿using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Map;
 using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
 using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement.Categories;
+using TaleWorlds.Core;
+using TaleWorlds.Core.ViewModelCollection;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using System.ComponentModel;
 
 namespace ManageRemoteCompanions
 {
@@ -30,4 +38,69 @@ namespace ManageRemoteCompanions
             return false;
         }
     }
+
+//    [HarmonyPatch(typeof(ClanPartiesVM), "OnPartySelection")]
+//    public class PatchClanPartiesVM
+//    {
+//        public static EventHandler<EventArgs> OnPartySelection;
+
+//        public static void Postfix()
+//        {
+//            OnPartySelection?.Invoke(null, new EventArgs());
+//        }
+//    }
+
+//    public class ManageTroopsVM : ViewModel
+//    {
+//        private ClanPartiesVM _clanPartiesVM;
+//        private bool _canManageTroops;
+
+//        public ManageTroopsVM(ClanPartiesVM clanPartiesVM)
+//        {
+//            _clanPartiesVM = clanPartiesVM;
+//            PatchClanPartiesVM.OnPartySelection += new EventHandler<EventArgs>(Refresh);
+//        }
+
+//        public void Refresh(object sender, EventArgs e)
+//        {
+//            CanManageTroops = calculateCanManageTroops(_clanPartiesVM.CurrentSelectedParty);
+//        }
+
+//        private bool calculateCanManageTroops(ClanPartyItemVM p)
+//        {
+//            if (p != null && p.Party != null && p.Party.MobileParty != null && Settings.Instance.ManageTroops)
+//            {
+//                if (p.Party.MobileParty.IsGarrison)
+//                    return Settings.Instance.ManageGarrisons;
+//                else if (p.Party.MobileParty.IsCaravan)
+//                    return Settings.Instance.ManageCaravans;
+//                else if (p.Party.MobileParty.LeaderHero != Hero.MainHero)
+//                    return Settings.Instance.ManageParties;
+//            }
+//            return false;
+//        }
+
+//        public void ExecuteManageTroops()
+//        {
+//            InformationManager.DisplayMessage(new InformationMessage("ClanPartiesMixin.ExecuteManageCurrentParty()"));
+//            if (CanManageTroops)
+//                PartyScreenManager.OpenScreenAsManageTroops(_clanPartiesVM.CurrentSelectedParty.Party.MobileParty);
+//        }
+
+//        [DataSourceProperty]
+//        public bool CanManageTroops
+//        {
+//            get
+//            {
+//                return _canManageTroops;
+//            }
+//            set
+//            {
+//                if (value == _canManageTroops)
+//                    return;
+//                _canManageTroops = value;
+//                OnPropertyChanged(nameof(CanManageTroops));
+//            }
+//        }
+//    }
 }
